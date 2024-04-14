fcf3dc390d20d771d3eb70aca3a9640270207a65
SuperGmaX <gmax9292@gmail.com>
2024-04-14 16:12:42 +0200

    maj test deploy

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
-export function Layout({ children }: { children: React.ReactNodeLike }) {
+export function Layout({ children }: { children: React.ReactNode }) {
   return <ThemeProvider>{children}</ThemeProvider>;
 }
 
